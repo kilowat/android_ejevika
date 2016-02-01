411b8cda9285299f7bd8de0e5da56e6a3f84af65
kilowat <kadantsev.sergey@gmail.com>
2016-02-01 22:01:20 +0300

    Replace file to activities package

--- app/src/main/java/com/example/razor/ejevika/acitvities/MainActivity.java
+++ app/src/main/java/com/example/razor/ejevika/acitvities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.razor.ejevika;
+package com.example.razor.ejevika.acitvities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.razor.ejevika.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
