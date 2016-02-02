124ee630676a38fcdc8af446a80c64bd5d182a79
kilowat <kadantsev.sergey@gmail.com>
2016-02-02 10:40:00 +0400

    add Category class

--- app/src/main/java/com/example/razor/ejevika/dummy/Category.java
+++ app/src/main/java/com/example/razor/ejevika/dummy/Category.java
@@ -4,4 +4,39 @@ package com.example.razor.ejevika.dummy;
  * Created by razor on 01.02.2016.
  */
 public class Category {
+
+    public int id;
+    public String picture;
+    public String name;
+
+    public Category(){}
+    public Category(int id, String picture, String name){
+        this.id = id;
+        this.picture = picture;
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
